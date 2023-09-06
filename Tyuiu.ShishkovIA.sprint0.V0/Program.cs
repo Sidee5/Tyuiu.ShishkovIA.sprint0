b6729fa514dab4ff4df51afe6d96c25dd97a7369
Tyuiu.ShishkovIA@outlook.com <ilja.shishkov.sidee5@gmail.com>
2023-09-06 16:15:02 +0500

    add task1

---
+++ Tyuiu.ShishkovIA.sprint0.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShishkovIA.sprint0.task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Илья";
+            int age = 20;                  // 14.11.2002
+            Console.WriteLine("Здраствуйте, меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+         
+        }
+    }
+}
